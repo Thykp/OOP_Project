88f98ec2d84b82ea01d61b090325ee048a444338
Thykp <101046365+Thykp@users.noreply.github.com>
2025-08-13 01:14:55 +0800

    fix Supabase connection pooling and test profile

--- backend/src/main/java/com/is442/backend/BackendApplication.java
+++ backend/src/main/java/com/is442/backend/BackendApplication.java
@@ -1,22 +1,11 @@
 package com.is442.backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
-
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure()
-                .directory("./")
-                .ignoreIfMissing()
-                .load();
-
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
         SpringApplication.run(BackendApplication.class, args);
     }
 }
--- backend/src/test/java/com/is442/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/is442/backend/BackendApplicationTests.java
@@ -2,12 +2,13 @@ package com.is442.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(
+		classes = BackendApplication.class,
+		webEnvironment = SpringBootTest.WebEnvironment.NONE
+)
+@ActiveProfiles("test")
 class BackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+	@Test void contextLoads() {}
 }
