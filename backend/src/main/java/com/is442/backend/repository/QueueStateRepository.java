0e4e9b5842c3a71a44c7b862f4f641634f146117
Kevin Tan <cekevintan@gmail.com>
2025-11-11 22:17:32 +0800

    deleted old queue infra

--- backend/src/main/java/com/is442/backend/repository/QueueStateRepository.java
+++
@@ -1,6 +0,0 @@
-package com.is442.backend.repository;
-
-import com.is442.backend.model.QueueState;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QueueStateRepository extends JpaRepository<QueueState, String> {}
